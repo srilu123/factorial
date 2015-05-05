b8b55732c31ed22a58eef77fb9cd76baf8e32ec2
Srilatha <srilu.kante04@gmail.com>
2015-05-06 02:02:08 +0530

    format

--- factorial-app/src/main/java/com/parsa/app/Point.java
+++ factorial-app/src/main/java/com/parsa/app/Point.java
@@ -1,11 +1,12 @@
 package com.parsa.app;
 
 public class Point {
-	int x=0;
-	int y=0;
-	public Point(int a, int b){
-		x=a;
-		y=b;
+	int x = 0;
+	int y = 0;
+
+	public Point(int a, int b) {
+		x = a;
+		y = b;
 	}
 
 }
--- factorial-app/src/main/java/com/parsa/app/Rectangle.java
+++ factorial-app/src/main/java/com/parsa/app/Rectangle.java
@@ -1,34 +1,38 @@
 package com.parsa.app;
 
 public class Rectangle {
-	int height=0;
-	int width=0;
+	int height = 0;
+	int width = 0;
 	float area;
 	Point origin;
-	public Rectangle(){
-		origin=new Point(0,0);
+
+	public Rectangle() {
+		origin = new Point(0, 0);
 	}
-	public Rectangle(Point p,int w,int h){
-		origin=p;
-		width=w;
-		height=h;
+
+	public Rectangle(Point p, int w, int h) {
+		origin = p;
+		width = w;
+		height = h;
 	}
-		public Rectangle(int w,int h)
-		{
-			width=w;
-			height=h;
-			
-		}
-		public void moveOrigin(int x,int y){
-			origin.x=x;
-			origin.y=y;
-		}
+
+	public Rectangle(int w, int h) {
+		width = w;
+		height = h;
+
+	}
+
+	public void moveOrigin(int x, int y) {
+		origin.x = x;
+		origin.y = y;
+	}
+
 	public int getArea()
-	
+
 	{
-		
-		return width*height;
-		
+
+		return width * height;
+
 	}
 
 }
