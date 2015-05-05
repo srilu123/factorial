ed84157130368c05608239ef3c22cd91b91b44c1
Srilatha <srilu.kante04@gmail.com>
2015-05-06 04:04:53 +0530

    point class using this keyword

--- factorial-app/src/main/java/com/parsa/app/App.java
+++ factorial-app/src/main/java/com/parsa/app/App.java
@@ -13,9 +13,18 @@ public class App
     	Rectangle rect1=new Rectangle(originOne,5,6);
     	Rectangle rect2=new Rectangle(10, 20);
     	
+    	int height=new Rectangle().height;
+    	
     	System.out.println("the width of the rectangle:"+rect1.width);
     	System.out.println("the height of the rectangle:"+rect1.height);
     	System.out.println("the area of rectangle:"+rect2.getArea());
+    	
+    	
+    	System.out.println(new Rectangle(100, 50).getArea());
+    	
+    	System.out.println(height);
+    	System.out.println(height=new Rectangle().height);
+    	
     	
     	rect2.origin=originOne;
     	System.out.println("X is"+rect2.origin.x);
--- factorial-app/src/main/java/com/parsa/app/Point.java
+++ factorial-app/src/main/java/com/parsa/app/Point.java
@@ -4,9 +4,9 @@ public class Point {
 	int x = 0;
 	int y = 0;
 
-	public Point(int a, int b) {
-		x = a;
-		y = b;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
 
 }
