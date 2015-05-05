15eeb9f12ed44994e0541d396ea664f0ba0dc083
Srilatha <srilu.kante04@gmail.com>
2015-05-06 01:58:55 +0530

    Rectangle

--- factorial-app/src/main/java/com/parsa/app/App.java
+++ factorial-app/src/main/java/com/parsa/app/App.java
@@ -9,6 +9,25 @@ public class App
     public static void main( String[] args )
     {
     	
+    	Point originOne=new Point(2,3);
+    	Rectangle rect1=new Rectangle(originOne,5,6);
+    	Rectangle rect2=new Rectangle(10, 20);
+    	
+    	System.out.println("the width of the rectangle:"+rect1.width);
+    	System.out.println("the height of the rectangle:"+rect1.height);
+    	System.out.println("the area of rectangle:"+rect2.getArea());
+    	
+    	rect2.origin=originOne;
+    	System.out.println("X is"+rect2.origin.x);
+    	System.out.println("Y is"+rect2.origin.y);
+    	
+    	rect2.moveOrigin(25, 30);
+    	System.out.println("X is"+rect2.origin.x);
+    	System.out.println("Y is"+rect2.origin.y);
+    	
+    	
+    	
+    	
     	Factorial factorial=new Factorial();
     	System.out.println(factorial.getFactorial(5));
     }
---
+++ factorial-app/src/main/java/com/parsa/app/Point.java
@@ -0,0 +1,11 @@
+package com.parsa.app;
+
+public class Point {
+	int x=0;
+	int y=0;
+	public Point(int a, int b){
+		x=a;
+		y=b;
+	}
+
+}
